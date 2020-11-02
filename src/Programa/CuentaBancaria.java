3ae69371b4a927cc06924455441cb4a53b77ee8b
Skorrom <73122602+Skorrom@users.noreply.github.com>
2020-11-02 16:30:52 -0300

    Subo metodo transaccionesIgualMonto y transaccionMasCostosa

--- src/Programa/CuentaBancaria.java
+++ src/Programa/CuentaBancaria.java
@@ -5,24 +5,29 @@ import TDAColaCP.EmptyPriorityQueueException;
 import TDAColaCP.InvalidKeyException;
 import TDAColaCP.PriorityQueue;
 import TDADeque.Deque;
+import TDADiccionario.DiccionarioHashAbierto;
+import TDADiccionario.Dictionary;
+import TDADiccionario.Entry;
+import TDALista.ListaDE;
+import TDALista.PositionList;
 
 public class CuentaBancaria {
 	protected float saldo;
-	protected Deque<Transaccion<Float,String>> transacciones;
+	protected Deque<Transaccion> transacciones;
 	
 	/**
 	 * Instancia una nueva cuenta bancaria con saldo en cero.
 	 */
 	public CuentaBancaria() {
 		saldo = 0.00F;
-		transacciones = new Deque<Transaccion<Float,String>>();
+		transacciones = new Deque<Transaccion>();
 	}
 	
 	/**
 	 * Método que permite obtener la transacción más reciente efectuada.
 	 * @return La Transaccion más reciente.
 	 */
-	public Transaccion<Float, String> transaccionMasReciente() {
+	public Transaccion transaccionMasReciente() {
 		return transacciones.getLast();
 	}
 	
@@ -30,7 +35,7 @@ public class CuentaBancaria {
 	 * Método que permite obtener la transacción más histórica efectuada.
 	 * @return La Transaccion más histórica.
 	 */
-	public Transaccion<Float, String> transaccionMasHistorica() {
+	public Transaccion transaccionMasHistorica() {
 		return transacciones.getFirst();
 	}
 	
@@ -38,50 +43,46 @@ public class CuentaBancaria {
 	 * Método que permite obtener la transacción más costosa efectuada.
 	 * @return La Transaccion más costosa.
 	 */
-	public Transaccion<Float, String> transaccionMasCostosa() {
-		Transaccion<Float, String> masCostosa = new Transaccion<Float, String>();
-		PriorityQueue<Integer, Transaccion<Float, String>> masCostosaCCP = new ColaCPconLista<Integer, Transaccion<Float, String>>();
-		float montoMayor = 0;
-		int prioridadActualMin = transacciones.size();
-		int prioridadActualMax = transacciones.size() + 1;
-		
+	public Transaccion transaccionMasCostosa() {
+		Transaccion masCostosa = new Transaccion();
+		PriorityQueue<Float, Transaccion> masCostosaCCP = new ColaCPconLista<Float, Transaccion>();
 		try {
-			
-			// Paso cada transaccion a la CCP procurando que la de menor
-			// prioridad sea la del monto más alto.
-			for (Transaccion<Float,String> t: transacciones) {
-				if (t.getMonto() > montoMayor) {
-					masCostosaCCP.insert(prioridadActualMin, t);
-					prioridadActualMin--;
-					montoMayor = t.getMonto();
-				} else {
-					masCostosaCCP.insert(prioridadActualMax, t);
-					prioridadActualMax++;
-				}
+			//Inserto todas las transacciones en la cola. Su prioridad (o monto) pasa a ser negativa asi las de mayor monto quedan primeras.
+			for (Transaccion t: transacciones) {
+				masCostosaCCP.insert(-t.getMonto(),t);
 			}
-		
 			masCostosa = masCostosaCCP.min().getValue();
-			
 		} catch (EmptyPriorityQueueException | InvalidKeyException e) {
 			e.printStackTrace();
 		}
-		
-		return (Transaccion<Float, String>) masCostosa;
+		return masCostosa;
 	}
 	
 	/**
 	 * Método que permite obtener las transacciones con igual monto.
 	 * @return Transacciones con igual monto.
 	 */
-	public Transaccion<Float, String> transaccionIgualMonto() {
-		return null;
+	public Iterable<Transaccion> transaccionesIgualMonto(Float monto) {
+		Dictionary<Float,Transaccion> transaccionesDiccionario = new DiccionarioHashAbierto<Float,Transaccion>();
+		PositionList<Transaccion> transaccionesLista = new ListaDE<Transaccion>();
+		try {
+			for(Transaccion t : transacciones) {
+				transaccionesDiccionario.insert(t.getMonto(), t);
+			}
+			for(Entry<Float, Transaccion> e : transaccionesDiccionario.findAll(monto)) {
+				transaccionesLista.addLast(e.getValue());
+			}
+		} catch (TDADiccionario.InvalidKeyException e) {
+			e.printStackTrace();
+		}
+		return transaccionesLista;
 	}
 	
 	/**
 	 * Método que efectúa una transacción t.
 	 * @param t transacción a computar.
 	 */
-	public void realizarTransaccion (Transaccion<Float, String> t) {
+	public void realizarTransaccion (Transaccion t) {
 		saldo += t.getMonto();
 		transacciones.add(t);
 	}
